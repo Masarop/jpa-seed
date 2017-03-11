5c47f4a09e5e2984b57f953664848a3af825f7bd
Masarop <lucasarrudadeoliveira@gmail.com>
2017-03-11 15:07:17 -0300

    entity venda

---
+++ src/main/java/br/edu/faculdadedelta/modelo/Venda.java
@@ -0,0 +1,54 @@
+package br.edu.faculdadedelta.modelo;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.ManyToAny;
+
+public class Venda extends BaseEntity<Long> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="id_venda")
+	private Long id;
+	
+	@ManyToOne(cascade = {CascadeType.PERSIST},fetch=FetchType.LAZY)
+	@JoinColumn(name="id_cliente",referencedColumnName="id_cliente",nullable=false,insertable=true,updatable=false)
+	private Cliente cliente;
+	
+	
+	
+	@Override
+	public Long getId() {
+		return null;
+	}
+
+
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+}
